058ea3aaa30359d351b0a7980f8d09d2b1a91d52
Philipp Aigner <if25b196@technikum-wien.at>
2026-06-09 11:13:21 +0200

    Test1 - normal item loses quality 1 every day

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using GildedRoseKata;
 using NUnit.Framework;
 
@@ -6,12 +6,15 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
+    // Test 1: Test if normal item loses 1 quality per day
     [Test]
-    public void Foo()
+    public void NormalItem_QualityDecreasesBy1_BeforeSellIn()
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 5, Quality = 10 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].Quality, Is.EqualTo(9));
     }
+
+    
 }
